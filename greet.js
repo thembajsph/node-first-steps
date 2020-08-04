0710de9292bb04cec932075c8d44b933c833b10d
thembajoseph <thembajsph1@gmail.com>
2020-08-04 14:32:07 +0200

    initial commit

---
+++ greet.js
@@ -0,0 +1,10 @@
+// function greet(sipho) {
+//     var message = 'Hello, ';
+//        return message + sipho };
+
+//        var msg = greet("Xola");
+//        console.log(msg);
+
+module.exports = function(name){
+    return 'Hello, ' + name;
+}
